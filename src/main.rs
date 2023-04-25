9afdb8cb0fd1378596b7665e5a71763490f895f8
Joshua Fitzgerald <joshuabfitzgerald@gmail.com>
2023-04-25 15:53:05 -0400

    fix paths in main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use vamp_ir::halo2;
-use vamp_ir::plonk;
+use vamp_ir::halo2::cli::{halo2, Halo2Commands};
+use vamp_ir::plonk::cli::{plonk, PlonkCommands};
 use clap::{Parser, Subcommand, ValueEnum};
 
 #[derive(Parser)]
@@ -12,9 +12,9 @@ struct Cli {
 #[derive(Subcommand)]
 enum Backend {
     #[command(subcommand)]
-    Plonk(plonk::cli::PlonkCommands),
+    Plonk(PlonkCommands),
     #[command(subcommand)]
-    Halo2(halo2::cli::Halo2Commands),
+    Halo2(Halo2Commands),
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
@@ -30,7 +30,7 @@ fn main() {
 
     let cli = Cli::parse();
     match &cli.backend {
-        Backend::Plonk(plonk_commands) => plonk::cli::plonk(plonk_commands),
-        Backend::Halo2(halo2_commands) => halo2::cli::halo2(halo2_commands),
+        Backend::Plonk(plonk_commands) => plonk(plonk_commands),
+        Backend::Halo2(halo2_commands) => halo2(halo2_commands),
     }
 }
