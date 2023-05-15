f9bd9056ed37f8d5a115422a1ac84fd75980c8da
lopeetall <joshuabfitzgerald@gmail.com>
2023-05-15 16:03:23 -0400

    add halo2 test example for range

--- src/test.rs
+++ src/test.rs
@@ -3,7 +3,7 @@ mod test {
     use std::{collections::HashMap, fs::File};
 
     #[test]
-    fn range_plonk() {
+    fn test_range_plonk() {
         use crate::plonk::cli::*;
 
         let setup = Setup {
@@ -20,6 +20,47 @@ mod test {
             unchecked: true,
         };
         compile_plonk_cmd(&compile);
+
+        let inputs_file = File::create("range.inputs").expect("Could not create inputs file");
+        let mut input_map = HashMap::new();
+        input_map.insert("x", "10");
+
+        serde_json::to_writer(inputs_file, &input_map).unwrap();
+
+        let prove = Halo2Prove {
+            universal_params: "test_params.pp".into(),
+            circuit: "range.plonk".into(),
+            output: "range.proof".into(),
+            unchecked: true,
+            inputs: Some("range.inputs".into()),
+        };
+        prove_plonk_cmd(&prove);
+
+        let verify = Halo2Verify {
+            universal_params: "test_params.pp".into(),
+            circuit: "range.plonk".into(),
+            proof: "range.proof".into(),
+            unchecked: true,
+        };
+        verify_plonk_cmd(&verify);
+    }
+
+    #[test]
+    fn test_range_halo2() {
+        use crate::halo2::cli::*;
+
+        // let setup = Setup {
+        //     max_degree: 10,
+        //     output: "test_params.pp".into(),
+        //     unchecked: true,
+        // };
+        // setup_plonk_cmd(&setup);
+
+        let compile = Halo2Compile {
+            source: "./tests/range.pir".into(),
+            output: "range.halo2".into(),
+        };
+        compile_halo2_cmd(&compile);
 
         let inputs_file = File::create("range.inputs").expect("Could not create inputs file");
         let mut input_map = HashMap::new();
