43186caa8b524a3a59e9851bb22982830d86a1ed
lopeetall <joshuabfitzgerald@gmail.com>
2023-05-17 15:49:44 -0400

    create error.rs with compile errors

---
+++ src/error.rs
@@ -0,0 +1,65 @@
+use crate::ast::{TExpr, TPat};
+
+#[derive(Debug)]
+pub enum Error {
+    // Parse errors
+
+    // Compilation errors
+
+    // cannot apply {} to {}
+    ApplicationError { expr2: TExpr, expr1: TExpr },
+
+    // enountered empty sequence
+    EmptySequenceError,
+
+    // variables are not permitted in expression exponents
+    VariableExponentError,
+
+    // only constant arguments to iter supported
+    NonConstantIterArgumentError,
+
+    // cannot statically match {} against {}
+    MatchError { expr: TExpr, pat: TPat },
+
+    // only list arguments to fold supported
+    NonListArgumentsInFoldError,
+
+    // encountered unexpected expression: {}
+    UnexpectedExpression { expr: TExpr },
+
+    // encountered unexpected pattern: {}
+    UnexpectedPattern { pat: TPat },
+
+    // only variable patterns should be present at this stage
+    UnexpectedNonVariablePattern,
+
+    // unexpected parameters for fresh: {:?}
+    UnexpectedFreshParameters { params: Vec<TPat> },
+
+    // unexpected arguments to iter: {:?}
+    UnexpectedIterArguments { params: Vec<TPat> },
+
+    // unexpected arguments to fold: {:?}
+    UnexpectedArgumentsInFold { params: Vec<TPat> },
+    // Type errors
+}
+
+// pub enum ParseError {
+
+// }
+
+// pub enum TypeError {
+
+// }
+
+// pub enum CompileError {
+
+// }
+
+// pub enum ProofError {
+//     TooFewParameters,
+// }
+
+// pub enum VerifyError {
+//     ProofVerificationError,
+// }
